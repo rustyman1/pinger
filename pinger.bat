3d0b6a716ec46256716bf929f06f37e00f4860ad
rustyman1 <67009570+rustyman1@users.noreply.github.com>
2020-09-29 14:48:06 -0400

    Add files via upload

---
+++ pinger.bat
@@ -0,0 +1,106 @@
+@echo off
+title bigfella#413 on discord pinger
+cls
+color 01
+echo MSGBOX "skid offline" > %temp%\TEMPmessage.vbs
+echo ****************
+echo *
+@echo off
+echo ``````````````````````````````````````````````````
+echo ``````````````````````````````````.....```````````
+echo ``````````````````````````````````.:/hyy/.````````
+echo ```````.-/+/.``````````````````````+yhhdhh:```````
+echo ``````````sss+.``.`..````````````.ohdhhhhhh-``````
+echo ``````````/sysssssoy+///:.``````/shdhyysyyyy-`````
+echo ```````````:+oyshhdhhhys/+::.``/yhhysssssssyo.````
+echo ``````````:sshyhyhhhyysyyyyy+/shhyoosososoosyo````
+echo ````````-+hhhhddhyyhysysoyhhdddhyyo/oo+o+o+osy/```
+echo ``````-yhhhyyyhhs+ssyyyhyyhhddhyyoo+:++/+/+++os.``
+echo ```./oyhyyyyhyss+:oosysyhhhhhhhhyo+o+/+//+++/oo.``
+echo ````ssoooyhoo+:..-/:o::yyshyysydh/--/:o+++++/+/```
+echo ``````.:so:-.`````````.oysossyhhy/````.-.+o++++```
+echo ``````-++.`````````````.yhooyhdh+.```````.-/oo+```
+echo ```````````````````````.++:/oydh:.``````````s/+```
+echo ````````````````````````````-+sdo.``````````o-:```
+echo `````````````````````````````/+ho.`````````./:````
+echo `````````````````````````````:oh/``````````.`.````
+echo `````````````````````````````/ys.`````````````````
+echo ````````````````..`````````.+ys-``````````````````
+echo ``````````````-+yyyso+/::/os+-````````````````````
+echo ````````````.oyhhy::///+/:.```````````````````````
+echo ````````````/:-```````````````````````````````````
+echo ``````````````````````````````````````````````````
+echo ``````````````````````````````````````````````````
+echo since this mostly used for ddsoing I AM NOT RESPONSIBLE
+echo FOR ANY TROUBLE YOU GET INTO WHILE USING MY SOFTWARE
+echo this is an open source project created by bigfella#8739 this ip pinger wil be put
+echo into are pannel once its done its a work in proggress dm me if ur interested
+echo ****************
+echo.                    
+set /p IP=Enter IP to offline check!::
+:top
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 call %temp%\TEMPmessage.vbs
+ping -t 2 0 10 127.0.0.1 >nul
+
+color 01
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 call %temp%\TEMPmessage.vbs
+ping -t 2 0 10 127.0.0.1 >nul
+color 02
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 call %temp%\TEMPmessage.vbs 
+ping -t 2 0 10 127.0.0.1 >nul
+color 03
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 call %temp%\TEMPmessage.vbs 
+ping -t 2 0 10 127.0.0.1 >nul
+color 04
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 call %temp%\TEMPmessage.vbs
+ping -t 2 0 10 127.0.0.1 >nul
+color 05
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 call %temp%\TEMPmessage.vbs 
+ping -t 2 0 10 127.0.0.1 >nul
+color 06
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 call %temp%\TEMPmessage.vbs 
+ping -t 2 0 10 127.0.0.1 >nul
+color 07
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 call %temp%\TEMPmessage.vbs 
+ping -t 2 0 10 127.0.0.1 >nul
+color 08
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 call %temp%\TEMPmessage.vbs 
+ping -t 2 0 10 127.0.0.1 >nul
+color 09
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 call %temp%\TEMPmessage.vbs
+ping -t 2 0 10 127.0.0.1 >nul
+color 0a
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 call %temp%\TEMPmessage.vbs
+ping -t 2 0 10 127.0.0.1 >nul
+color 0b
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 call %temp%\TEMPmessage.vbs 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0c
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 call %temp%\TEMPmessage.vbs 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0d
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 call %temp%\TEMPmessage.vbs
+ping -t 2 0 10 127.0.0.1 >nul
+color 0e
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 call %temp%\TEMPmessage.vbs
+ping -t 2 0 10 127.0.0.1 >nul
+color 0f
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 call %temp%\TEMPmessage.vbs
+ping -t 2 0 10 127.0.0.1 >nul
+goto top
